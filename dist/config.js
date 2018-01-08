c8334fdfa535b4768ba4a7055e9b966b59ecef76
Kiyonori Matsumoto <kiyonori.matsumoto@gmail.com>
2018-01-08 16:23:20 +0900

    update ci

--- dist/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Config;
-(function (Config) {
-    Config.endpoint = 'https://api.zaif.jp/';
-})(Config = exports.Config || (exports.Config = {}));
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './private';
-export * from './privatev2';
-export * from './leverage';
-export * from './public';
-export * from './config';
